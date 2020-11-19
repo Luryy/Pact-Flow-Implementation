006093b078c9061787e30e08a455ccb5c3f0c87a
Lucas Yuri <lucasyuri56@hotmail.com>
2020-11-19 11:57:01 -0300

    remove: remove repositories

--- src/repositories/IRolesRepository.ts
+++
@@ -1,8 +0,0 @@
-import Role from '../interfaces/Role';
-
-export default interface IRolesRepository {
-    create(data: Role): Promise<Role>;
-    findById(role_id: string, populate?: boolean): Promise<Role | null>;
-    save(role: Role): Promise<Role>;
-    delete(role_id: string): Promise<void>;
-}
